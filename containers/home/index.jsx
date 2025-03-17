4695f9f40a906d5593abc678c5ef604189816149
Öykü Kaya <oykuky001@gmail.com>
2025-03-18 01:15:16 +0300

    feat: videoPart component added

--- containers/home/index.jsx
+++ containers/home/index.jsx
@@ -1,6 +1,8 @@
 import InfoSection from "@/components/home/infoSection";
+import PartyAddOnsSection from "@/components/home/partyAddOnsSection";
 import PartyPackagesSection from "@/components/home/partyPackagesSection";
 import SlideSection from "@/components/home/slideSection";
+import VideoPart from "@/components/home/videoPart";
 
 export default function HomePageContainer() {
     return(
@@ -8,6 +10,8 @@ export default function HomePageContainer() {
           <SlideSection/>
           <InfoSection/>
           <PartyPackagesSection/>
+          <PartyAddOnsSection/>
+          <VideoPart/>
         </>
     );
 }
