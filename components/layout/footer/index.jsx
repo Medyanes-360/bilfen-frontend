521e944e9edf3cb9e4faf83386e7a59382e7bee5
Öykü Kaya <oykuky001@gmail.com>
2025-03-17 15:43:38 +0300

    footer updated

--- components/layout/footer/index.jsx
+++ components/layout/footer/index.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="flex items-center justify-center w-full bg-[#331d74] text-white py-6 h-36">
-        <div className="text-xl flex-row justify-center items-center">
-            <p className="font-normal text-center">
+        <div className="text-lg flex-row justify-center items-center">
+            <p className="font-light text-center">
               <span className="text-orange">ThemeREX</span> © 2025. All Rights Reserved.
             </p>
         </div>
