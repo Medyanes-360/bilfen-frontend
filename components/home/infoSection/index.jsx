c4541f63d71b3d97a7b50ed02bb1ee996026a5fd
Öykü Kaya <oykuky001@gmail.com>
2025-03-18 14:52:06 +0300

    font size update

--- components/home/infoSection/index.jsx
+++ components/home/infoSection/index.jsx
@@ -2,11 +2,15 @@ import PageContainer from "@/containers/pageContainer";
 import React from "react";
 import TextPart from "./textPart";
 import Icons from "@/public/icons/Icons";
+import Image from "next/image";
 
 const InfoSection = () => {
   return (
     <PageContainer>
-      <div className="flex flex-col lg:flex-row gap-12 py-12">
+      <div id="next-section" className="z-20 relative flex flex-col lg:flex-row gap-12 py-16 xl:py-36 lg:h-screen xl:h-full">
+      <div className="absolute top-[59%] left-[4%] w-14 h-14 hidden md:block">
+        <Image src="/images/star4.png" alt="Star" width={36} height={46} />
+      </div>
         <TextPart />
         {/* ImagePart */}
         <div className="relative z-10 rounded-3xl sm:bg-[url('/images/happy.png')] bg-center bg-no-repeat bg-cover w-full h-[410px]">
