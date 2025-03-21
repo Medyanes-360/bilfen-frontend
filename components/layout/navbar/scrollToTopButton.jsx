af766c8f86908e9fef164ae80d35703699c68b74
Öykü Kaya <oykuky001@gmail.com>
2025-03-21 17:24:38 +0300

    scroll button update

--- components/layout/navbar/scrollToTopButton.jsx
+++ components/layout/navbar/scrollToTopButton.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import Icons from "@/public/icons/Icons";
 
 const ScrollToTopButton = ({ show }) => {
   const scrollToTop = () => {
@@ -9,29 +9,15 @@ const ScrollToTopButton = ({ show }) => {
   };
 
   return (
-    <Button
+    <div
       onClick={scrollToTop}
-      className={`fixed bottom-8 right-8 bg-orange hover:text-darklila text-white rounded-full w-18 h-18 flex items-center justify-center shadow-lg transition-all duration-300 z-50 ${
+      className={`cursor-pointer w-18 h-18 fixed bottom-8 right-8 bg-orange rounded-full flex items-center justify-center shadow-lg transition-all duration-300 z-50 ${
         show ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10 pointer-events-none'
       }`}
       aria-label="Scroll to top"
     >
-      <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        width="24" 
-        height="24" 
-        viewBox="0 0 24 24" 
-        fill="none" 
-        stroke="currentColor" 
-        strokeWidth="2" 
-        strokeLinecap="round" 
-        strokeLinejoin="round"
-      >
-        <circle cx="12" cy="12" r="10" />
-        <polyline points="16 12 12 8 8 12" />
-        <line x1="12" y1="16" x2="12" y2="8" />
-      </svg>
-    </Button>
+     <Icons.UpIcon className="text-white hover:text-darklila w-12 h-12 duration-500 transition"/>
+    </div>
   );
 };
 
--- public/icons/Icons.jsx
+++ public/icons/Icons.jsx
@@ -528,14 +528,14 @@ export default class Icons {
 
   static Instagram({ className }) {
     return (
-      <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        viewBox="0 0 24 24" 
-        fill="none" 
-        stroke="currentColor" 
-        strokeWidth="2" 
-        strokeLinecap="round" 
-        strokeLinejoin="round" 
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className={className}
       >
         <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
@@ -547,14 +547,14 @@ export default class Icons {
 
   static Twitter({ className }) {
     return (
-      <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        viewBox="0 0 24 24" 
-        fill="none" 
-        stroke="currentColor" 
-        strokeWidth="2" 
-        strokeLinecap="round" 
-        strokeLinejoin="round" 
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className={className}
       >
         <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
@@ -564,14 +564,14 @@ export default class Icons {
 
   static Facebook({ className }) {
     return (
-      <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        viewBox="0 0 24 24" 
-        fill="none" 
-        stroke="currentColor" 
-        strokeWidth="2" 
-        strokeLinecap="round" 
-        strokeLinejoin="round" 
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className={className}
       >
         <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
@@ -579,6 +579,37 @@ export default class Icons {
     );
   }
 
-
-
+  static UpIcon({ className }) {
+    return (
+      <svg
+        className={className}
+        width="86px"
+        height="86px"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g id="SVGRepo_bgCarrier" strokeWidth="1"></g>
+        <g
+          id="SVGRepo_tracerCarrier"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+          {" "}
+          <g id="Shape / Triangle">
+            {" "}
+            <path
+              id="Vector"
+              d="M4.37891 15.1999C3.46947 16.775 3.01489 17.5634 3.08281 18.2097C3.14206 18.7734 3.43792 19.2851 3.89648 19.6182C4.42204 20.0001 5.3309 20.0001 7.14853 20.0001H16.8515C18.6691 20.0001 19.5778 20.0001 20.1034 19.6182C20.5619 19.2851 20.8579 18.7734 20.9172 18.2097C20.9851 17.5634 20.5307 16.775 19.6212 15.1999L14.7715 6.79986C13.8621 5.22468 13.4071 4.43722 12.8135 4.17291C12.2957 3.94236 11.704 3.94236 11.1862 4.17291C10.5928 4.43711 10.1381 5.22458 9.22946 6.79845L4.37891 15.1999Z"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>{" "}
+          </g>{" "}
+        </g>
+      </svg>
+    );
+  }
 }
