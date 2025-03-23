5a2fc3789eb088e027e7830d5c4eba2bb99df6ef
Vugar <tu201906231@code.edu.az>
2025-03-23 17:18:33 +0400

    created-hero-&-guide-sections

---
+++ components/howItWorks/guideSection/index.jsx
@@ -0,0 +1,97 @@
+"use client";
+import PageContainer from "@/containers/pageContainer";
+import { Parallax } from "@/globalElements/Parallax";
+import Image from "next/image";
+import dynamic from "next/dynamic";
+
+// Prevent hydration mismatch warning
+const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
+
+const GuideSection = () => {
+  return (
+    <div className="bg-[#fff6e9]">
+      <PageContainer className="px-3 md:px-6">
+        <div id="next-section" className="group z-20 relative py-16 xl:py-36">
+          <Parallax
+            sensitivity={0.05}
+            className="absolute md:top-[9%] top-[12%] right-[1%] hidden lg:block"
+          >
+            <Image src="/images/star4.png" alt="Star" width={36} height={46} />
+          </Parallax>
+          <Parallax
+            sensitivity={0.05}
+            className="absolute top-[62%] md:top-[59%] left-[4%] xxl:left-[0.2%] w-14 h-14 transition-transform duration-400 group-hover:-translate-x-2 group-hover:translate-y-2 hidden lg:block"
+          >
+            <Image src="/images/star4.png" alt="Star" width={36} height={46} />
+          </Parallax>
+          <Parallax
+            sensitivity={0.05}
+            className="absolute bottom-[9%] right-[1%] w-14 h-14 hidden xl:block transition-transform duration-400 group-hover:-translate-x-2 group-hover:translate-y-2"
+          >
+            <Image src="/images/star4.png" alt="Star" width={36} height={46} />
+          </Parallax>
+          <Parallax
+            sensitivity={0.03}
+            rotate={6}
+            className="absolute top-[23%] 2xl:-left-[19%] -left-8 hidden xxl:block"
+          >
+            <Image
+              src="/images/rocket3.png"
+              alt="Rocket"
+              width={100}
+              height={200}
+            />
+          </Parallax>
+          <div className="flex flex-col lg:flex-row justify-between w-full mb-10 lg:mb-6">
+            <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-6 md:mb-4">
+              <span className="text-[#ff621f]">Bilfen</span> System Usage
+            </h2>
+            <div className="pl-4 border-l border-[#ff621f] lg:max-w-[500px]">
+              <p className="text-lg md:text-xl text-[#595959] font-semibold">
+                We've prepared a compelling video containing every step to help
+                you start your journey in our wonderful system.
+              </p>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 lg:grid-cols-5 gap-4 place-items-center">
+            {/* Image */}
+            <div className="relative md:col-span-2 z-20 order-2 lg:order-1">
+              <Image
+                src="/images/kid-w-laptop.png"
+                width={492}
+                height={516}
+                alt="kid-w-laptop"
+                objectFit="cover"
+              />
+            </div>
+
+            {/* ReactPlayer Video */}
+            <div className="md:col-span-3 w-full h-full max-h-[350px] order-1 lg:order-2">
+              <div className="relative overflow-hidden rounded-xl shadow-lg bg-black">
+                <ReactPlayer
+                  url="/example-video.mp4"
+                  controls
+                  width="100%"
+                  height="100%"
+                  config={{
+                    file: {
+                      attributes: {
+                        controlsList: "nodownload",
+                        disablePictureInPicture: true,
+                        playsInline: true,
+                        preload: "auto",
+                      },
+                    },
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </PageContainer>
+    </div>
+  );
+};
+
+export default GuideSection;
---
+++ components/ui/breadCrumbs.jsx
@@ -0,0 +1,110 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+// Function to capitalize each word in a string
+const capitalizeWords = (str) => {
+  return str
+    .split("-")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+};
+
+export default function BreadCrumbComponent({ customPaths = [] }) {
+  const paths = usePathname();
+  const pathNames = paths.split("/").filter((path) => path);
+
+  if (pathNames.length === 0) {
+    return null;
+  }
+
+  const breadCrumbData = [];
+  // Add default paths to breadCrumbData
+  pathNames.forEach((elem, index) => {
+    breadCrumbData.push({
+      name: capitalizeWords(elem), // Apply capitalizeWords function here
+      url: `/${pathNames.slice(0, index + 1).join("/")}`,
+    });
+  });
+
+  // Handle customPaths
+  customPaths.forEach((elem) => {
+    if (elem.replace) {
+      breadCrumbData[elem.index - 1] = {
+        name: elem.name,
+        url: elem.url,
+      };
+    } else {
+      breadCrumbData.splice(elem.index, 0, {
+        name: elem.name,
+        url: elem.url,
+      });
+    }
+  });
+
+  return (
+    <div className="flex justify-center items-center pb-2 text-sm w-full">
+      <ul className="flex justify-center items-center gap-4 sm:gap-2 flex-wrap">
+        <li className="hover:text-[#8E89C0] transition-colors duration-200">
+          <Link
+            href={"/"}
+            className="text-base sm:text-[18px] leading-[30px] font-thin md:font-normal"
+          >
+            Home
+          </Link>
+        </li>
+        {pathNames.length > 0 && (
+          <span className="flex  items-center justify-center">
+            {" "}
+            <svg
+              width="4"
+              height="4"
+              viewBox="0 0 4 4"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="2" cy="2" r="2" fill="#919EAB" />
+            </svg>
+          </span>
+        )}
+        {breadCrumbData.map((elem, index) => {
+          let url = elem.url;
+
+          let name = elem.name;
+          let itemClasses =
+            paths === url
+              ? `text-black-disabled truncate [&_a]:cursor-default `
+              : "hover:underline mx-2 truncate";
+
+          return (
+            <React.Fragment key={index}>
+              <li className={itemClasses}>
+                <Link
+                  className="text-base sm:text-[18px] leading-[30px] font-thin md:font-normal"
+                  href={url}
+                >
+                  {name}
+                </Link>
+              </li>
+              {breadCrumbData.length !== index + 1 && (
+                <span className="flex items-center justify-center">
+                  {" "}
+                  <svg
+                    width="4"
+                    height="4"
+                    viewBox="0 0 4 4"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle cx="2" cy="2" r="2" fill="#919EAB" />
+                  </svg>
+                </span>
+              )}
+            </React.Fragment>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
